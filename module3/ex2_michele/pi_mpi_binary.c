3aa10f146ad5d173ae173cc3b6b1657080237c49
Michele <micpel@kth.se>
2020-10-02 21:54:53 +0200

    Added MPI binary tree MC calculation of pi

---
+++ module3/ex2_michele/pi_mpi_binary.c
@@ -0,0 +1,99 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+// Include MPI
+#include <mpi.h>
+
+#define MASTER 0
+
+#define DEFAULT_TAG 0
+
+#ifndef RNG_OFFSET
+#define RNG_OFFSET 1234
+#endif
+
+#ifndef SEED
+#define SEED 921
+#endif
+
+#ifndef NUM_ITER
+#define NUM_ITER 1000000000
+#endif
+
+int main()
+{
+	
+	// Initialize message passing
+	MPI_Init(nullptr, nullptr);
+
+	// Getting rank and size
+	int rank, size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	int local_num_iter = NUM_ITER/size;
+    	int count = 0;
+    	double x, y, z, pi;
+    
+	// Important: Multiply SEED by "rank" when you introduce MPI!
+    	srand(SEED*rank+RNG_OFFSET);
+    
+    	// Calculate PI following a Monte Carlo method
+    	for ( int iter = 0; iter < local_num_iter; iter++ )
+    	{
+        	// Generate random (X,Y) points
+        	x = (double)random() / (double)RAND_MAX;
+        	y = (double)random() / (double)RAND_MAX;
+        	z = sqrt((x*x) + (y*y));
+        	// Check if point is in unit circle
+        	if (z <= 1.0)
+        	{
+            		count++;
+        	}
+    	}
+
+	// Estimate Pi and display the result
+    	pi = ((double)count / (double)NUM_ITER) * 4.0;
+
+	double pi_mpi = pi;
+
+	int M = size;
+	while ( M>1 )
+	{
+		// DEBUG
+		M = M/2;
+		if (rank==MASTER)
+			printf("M = %d\n", M);
+
+		for ( int i=0; i<M; ++i )
+		{
+			if (rank==i)
+			{
+				// Recv
+				MPI_Recv(&pi_mpi, 1, MPI_DOUBLE, i+M, DEFAULT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				pi += pi_mpi;
+				pi_mpi = pi;
+			}
+			else if (rank==i+M)
+			{
+				// Send
+				MPI_Send(&pi_mpi, 1, MPI_INT, i, DEFAULT_TAG, MPI_COMM_WORLD);
+			}
+		}
+
+	}	
+    	
+	if ( rank==MASTER )
+    		printf("The result is %f\n", pi);
+
+	// Finalize message passing
+	MPI_Finalize();
+
+    	return 0;
+}
+
